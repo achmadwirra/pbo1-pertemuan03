02104778c43eebb039feb3de4b6bf41d9335ab25
achmadwirra <achmad.wirra@gmail.com>
2021-04-17 15:43:22 +0800

    Initial commit

---
+++ src/string/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+
+        String kota = "Banjarbaru";
+        int jumlah;
+        double nilai = 89.3;
+
+        char[] kotaArray = {'M','a','r','t','a','p'};
+
+        System.out.println(kota);
+        System.out.println(kotaArray);
+    }
+}
